684aef65544326096f49321ad9f4009deb7b6396
unknown <patidaranil0791@gmail.com>
2022-02-28 13:19:51 +0530

    CRUD APP

---
+++ db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const DB = process.env.DATABASE
+
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(()=> console.log("DataBase connected"))
+.catch((error)=> console.log(error.message));
